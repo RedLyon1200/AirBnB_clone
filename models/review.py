ee487841f156a84171bd543b25863169147898c1
Santiago Agudelo <sagudelo1200@gmail.com>
2020-06-28 03:49:12 +0000

    all classes added

---
+++ models/review.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is the Review class"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """[summary]
+    """
+    place_id = ''
+    user_id = ''
+    text = ''
+
+    pass
