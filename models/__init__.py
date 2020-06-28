48fb552cc8727a4ec4f49407d703d8c350d232fc
Santiago Agudelo <sagudelo1200@gmail.com>
2020-06-28 03:48:45 +0000

    all classes added

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""INIT MODELS"""
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
@@ -7,6 +8,8 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+
+
 classes = {
     'BaseModel': BaseModel,
     'User': User,
