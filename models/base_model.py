1b52dad77a962b25a15a420ab8b49f3b30092d8c
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-28 22:23:57 +0000

    fix basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -20,13 +20,17 @@ class BaseModel:
         """
 
         if kwargs:
-            self.__dict__ = kwargs
-            if 'created_at' in kwargs:
-                self.__dict__['created_at'] = datetime.strptime(
-                    kwargs.get('created_at'), self.format_date)
-            if 'updated_at' in kwargs:
-                self.__dict__['updated_at'] = datetime.strptime(
-                    kwargs.get('updated_at'), self.format_date)
+            for key, value in kwargs.items():
+                if 'created_at' is key:
+                    self.__dict__['created_at'] = datetime.strptime(
+                        kwargs.get('created_at'), self.format_date)
+                elif 'updated_at' is key:
+                    self.__dict__['updated_at'] = datetime.strptime(
+                        kwargs.get('updated_at'), self.format_date)
+                elif '__class__' is key:
+                    pass
+                else:
+                    self.__dict__[key] = value
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
